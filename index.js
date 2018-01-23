4c11d211e19ef7a2739dfac748d6a46edf2c0d46
CNBlackJ <yes.heng@icloud.com>
2018-01-23 11:05:27 +0800

    add pm2.json

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const egg = require('egg');
+const cluster = require('cluster');
 
-const workers = Number(process.argv[2] || require('os').cpus().length);
+const workers = 1; // Number(process.argv[2] || require('os').cpus().length);
+
 egg.startCluster({
   workers,
   baseDir: __dirname,
 });
+for (let i = 0; i < workers; i++) {
+  cluster.fork();
+}
