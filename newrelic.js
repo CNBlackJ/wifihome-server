e545219f66adfeb35fc2e0cef68c89cb947f6ead
CNBlackJ <yes.heng@icloud.com>
2018-01-24 14:18:18 +0800

    add new relic

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict';
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: [ 'wifihomw-server' ],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'e0fd535b4493c2af55a790c4824679a5ec5aa353',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info',
+  },
+};
